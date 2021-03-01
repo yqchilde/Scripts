97219de79c092fd5b1651c51815e82f48a18bafb
yqchilde <yqchilde@gmail.com>
2021-03-01 23:44:22 +0800

    no message

--- node/jd/deploy.sh
+++ node/jd/deploy.sh
@@ -95,7 +95,7 @@ function initScript() {
       docker cp ./jd_shake.js "$image":/scripts/jd_live_redrain.js
 
       echo -e "${GREEN}------------------------------------------------Exec Npm Install------------------------------------------------${RES}"
-      docker exec -it "$image" /bin/bash -c 'npm config set registry https://registry.npm.taobao.org && npm install'
+      docker exec "$image" sh 'npm config set registry https://registry.npm.taobao.org && npm install'
     }&
 
   done
