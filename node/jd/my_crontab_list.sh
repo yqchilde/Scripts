052683a74b2bef45cd32f79d0b00f6122a02f8e5
Yqchilde <yqchilde@gmail.com>
2021-03-03 08:22:39 +0800

    Update my_crontab_list.sh

--- node/jd/my_crontab_list.sh
+++ node/jd/my_crontab_list.sh
@@ -1,35 +1,26 @@
 # 兑换500豆子
-0 */3 * * * wget https://raw.githubusercontent.com/yqchilde/Scripts/main/node/jd/author/lxk0301/joy_reword.js -O /scripts/joy_reword.js
 3,6,9 0 8,16 * * * node /scripts/joy_reward.js >> /scripts/logs/joy_reward.log 2>&1
 
 # 母婴-跳一跳
-1 */3 * * * wget https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_jump-jump.js -O /scripts/jd_jump-jd_jump.js
 5 8,14,20 2-7 3 * node /scripts/jd_jump-jump.js >> /scripts/logs/jd_jump-jump.log 2>&1
 
 # 百变大咖秀
-3 */3 * * * wget https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_entertainment.js -O /scripts/jd_entertainment.js
 10 10,11 * * 2-5 node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
 
 # 粉丝互动
-4 */3 * * * wget https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_fanslove.js -O /scripts/jd_fanslove.js
 3 10 * * * node /scripts/jd_fanslove.js >> /scripts/jd_fanslove.log 2>&1
 
 # 财富岛通知
-6 */3 * * * wget https://raw.githubusercontent.com/moposmall/Script/main/Me/jx_cfd_exchange.js -O /scripts/jx_cfd_exchange.js
 30 6,12,22 * * * node /scripts/jx_cfd_exchange.js >> /scripts/logs/jx_cfd_exchange.log 2>&1
 
 # 摇一摇活动
-7 */3 * * * wget https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_shake.js -O /scripts/jd_shake.js
 3 20 * * * node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1
 
 # 财富岛提现
-8 */3 * * * https://raw.githubusercontent.com/yqchilde/Scripts/main/node/jd/author/whyour/jx_cfdtx.js -O /scripts/jx_cfdtx.js
 0 0 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
 
 # 红包雨
-9 */3 * * * https://raw.githubusercontent.com/yqchilde/Scripts/main/node/jd/author/lxk0301/jd_live_redrain.js -O /scripts/jd_live_redrain.js
 30,31 20-23/1 2,5 3 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 
 # 格式化互助码
-0 */12 * * * wget https://gitee.com/qq34347476/quantumult-x/raw/master/format_share_jd_code.js -O /scripts/format_share_jd_code.js
 0 1 0/2 * * node /scripts/format_share_jd_code.js >> /scripts/logs/format_share_jd_code.log 2>&1
