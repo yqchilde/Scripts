8239bdcef3affa1f0ed2501fd938b38439f9f7f4
yqchilde <yqchilde@gmail.com>
2021-03-06 20:52:56 +0800

    no message

--- node/jd/jd_script.sh
+++ node/jd/jd_script.sh
@@ -18,7 +18,7 @@ cp $(find /ybRepo/node/jd/author -type f -name "*.js") /scripts/
 {
 
   printf "# 兑换500豆子\n3,6,9 0 8,16 * * * node /scripts/joy_reward.js >> /scripts/logs/joy_reward.log 2>&1\n"
-  printf "# 母婴-跳一跳\n5 8,14,20 2-7 3 * node /scripts/jd_jump-jump.js >> /scripts/logs/jd_jump-jump.log 2>&1\n"
+  printf "# 母婴-跳一跳\n5 8,14,20 2-7 3 * node /scripts/jd_jump_jump.js >> /scripts/logs/jd_jump_jump.log 2>&1\n"
   printf "# 百变大咖秀\n10 10,11 * * 2-5 node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1\n"
   printf "# 粉丝互动\n3 10 * * * node /scripts/jd_fanslove.js >> /scripts/jd_fanslove.log 2>&1\n"
   printf "# 财富岛通知\n30 6,12,22 * * * node /scripts/jx_cfd_exchange.js >> /scripts/logs/jx_cfd_exchange.log 2>&1\n"
