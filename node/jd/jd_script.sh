6d86252e5eb1e77e88839f27d5307e7731319af6
yqchilde <yqchilde@gmail.com>
2021-03-12 08:16:07 +0800

    no message

--- node/jd/jd_script.sh
+++ node/jd/jd_script.sh
@@ -26,5 +26,5 @@ cp $(find /ybRepo/node/jd/author -type f -name "*.js") /scripts/
   printf "# 京东超市-大转盘\n3 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1\n"
   printf "# 洗护发超级品类日\n13 8,10 8-15 3 *  node /scripts/z_superDay.js >> /scripts/logs/z_superDay.log 2>&1\n"
   printf "# 美的家电节\n15 10 10-31 3 *  node /scripts/z_unionPoster.js >> /scripts/logs/z_unionPoster.log 2>&1\n"
-  printf "# 京东小魔方\n10 10 * * *  node /scripts/z_unionPoster.js >> /scripts/logs/z_unionPoster.log 2>&1\n"
+  printf "# 京东小魔方\n10 10 * * *  node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.log 2>&1\n"
 } >> /scripts/docker/merged_list_file.sh
