abfd99d427daed36c7950f22cc3d57d0ae00b27f
yqchilde <yqchilde@gmail.com>
2021-04-23 09:34:21 +0800

    no message

--- jd/jd_script.sh
+++ jd/jd_script.sh
@@ -16,19 +16,17 @@ fi
 cp $(find /ybRepo/jd/scripts/author -type f -name "*.js") /scripts/
 
 {
-  printf "# 手机狂欢城\n2 0-18/6 1-20 4 * node /scripts/z_carnivalcity.js >> /scripts/logs/z_carnivalcity.log 2>&1\n"
   printf "# 百变大咖秀\n10 10,11 * * 2-5 node /scripts/z_entertainment.js >> /scripts/logs/z_entertainment.log 2>&1\n"
   printf "# 粉丝互动\n3 10 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1\n"
   printf "# 健康社区\n25 10-22/3 * * * node /scripts/z_health_community.js >> /scripts/logs/z_health_community.log 2>&1\n"
   printf "# 健康社区收取能量\n25 * * * * node /scripts/z_health_energy.js >> /scripts/logs/z_health_energy.log 2>&1\n"
-  printf "# 京东超市-大转盘\n3 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1\n"
-  printf "# 母婴跳一跳\n5 8,14,20 20-27 4 * node /scripts/z_mother_jump.js >> /scripts/logs/z_mother_jump.log 2>&1\n"
+  printf "# 京东超市-大转盘\n3 10 * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1\n"
+  printf "# 母婴跳一跳\n5 8,14,20 * 4 * node /scripts/z_mother_jump.js >> /scripts/logs/z_mother_jump.log 2>&1\n"
   printf "# 超级摇一摇\n3 20 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1\n"
   printf "# 5G超级盲盒\n5 1,6,11,16,21 * 3-4 * node /scripts/z_super5g.js >> /scripts/logs/z_super5g.log 2>&1\n"
   printf "# 京东小魔方\n10 10 7-9 4 * node /scripts/z_xmf.js >> /scripts/logs/z_xmf.log 2>&1\n"
   printf "# interCenter渠道店铺签到\n0 0 * * * node /scripts/monk_inter_shop_sign.js >> /scripts/logs/monk_inter_shop_sign.log 2>&1\n"
   printf "# 有机牧场\n0 0,1-22/2 1-31 4-7 * node /scripts/monk_pasture.js >> /scripts/logs/monk_pasture.log 2>&1\n"
-  printf "# 店铺加购有礼\n15 12 * * * node /scripts/monk_shop_add_to_car.js >> /scripts/logs/monk_shop_add_to_car.log 2>&1\n"
   printf "# 店铺关注有礼\n15 15 * * * node /scripts/monk_shop_follow_sku.js >> /scripts/logs/monk_shop_follow_sku.log 2>&1\n"
   printf "# 店铺大转盘\n3 0,10,23 * * * node /scripts/monk_shop_lottery.js >> /scripts/logs/monk_shop_lottery.log 2>&1\n"
   printf "# 下班全勤奖\n15 08 2-18 4 * node /scripts/monk_skyworth_car.js >> /scripts/logs/monk_skyworth_car.log 2>&1\n"
