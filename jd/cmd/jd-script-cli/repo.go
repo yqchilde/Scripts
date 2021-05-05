acad7ad6c34ba23f9d9a88df932f9c56647ab483
yqchilde <yqchilde@gmail.com>
2021-05-05 14:14:37 +0800

    注掉一些冲突脚本cron

--- jd/cmd/jd-script-cli/repo.go
+++ jd/cmd/jd-script-cli/repo.go
@@ -60,6 +60,8 @@ const (
 func GetScriptTemplate() string {
 	return `#!/bin/bash
 
+mergedListFile="/scripts/docker/merged_list_file.sh"
+
 # 更新脚本
 if [ ! -d "/ybRepo/" ]; then
    echo "未检查到ybRepo仓库脚本，初始化下载相关脚本"
@@ -80,7 +82,12 @@ cp $(find /ybRepo/jd/scripts/author -type f -name "*.js") /scripts/
   {{- end }}
   printf "# 东东超市\n59,29 23,0 * * * sleep 57; node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1\n"
   printf "# 京东汽车兑换\n0,1,3,59 23,0 * * * sleep 57; node /scripts/jd_car.js >> /scripts/logs/jd_car.log 2>&1\n"
-} >> /scripts/docker/merged_list_file.sh
+} >> ${mergedListFile}
+
+# 修改定时任务
+sed -i 's/^0,30 0 \* \* \* node \/scripts\/jd_blueCoin.js/#&/' ${mergedListFile}
+sed -i 's/^0 0 \* \* \* node \/scripts\/jd_car.js/#&/' ${mergedListFile}
+sed -i 's/^1,31 0-23/1 \* \* \* node \/scripts\/jd_live_redrain.js/#&/' ${mergedListFile}
 `
 }
 
--- jd/jd_script.sh
+++ jd/jd_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mergedListFile="/scripts/docker/merged_list_file.sh"
+
 # 更新脚本
 if [ ! -d "/ybRepo/" ]; then
    echo "未检查到ybRepo仓库脚本，初始化下载相关脚本"
@@ -22,17 +24,19 @@ cp $(find /ybRepo/jd/scripts/author -type f -name "*.js") /scripts/
   printf "# 母婴跳一跳\n5 8,14,20 6-12 5 * node /scripts/diy_mother_jump.js >> /scripts/logs/diy_mother_jump.log 2>&1\n"
   printf "# 超级摇一摇\n3 20 * * * node /scripts/diy_shake.js >> /scripts/logs/diy_shake.log 2>&1\n"
   printf "# 超级无线组队分京豆\n25 3 * * 2 node /scripts/diy_shop_captain.js >> /scripts/logs/diy_shop_captain.log 2>&1\n"
-  printf "# 5G超级盲盒\n5 1,6,11,16,21 * 3-4 * node /scripts/diy_super5g.js >> /scripts/logs/diy_super5g.log 2>&1\n"
   printf "# 京东小魔方\n10 10 * 4 * node /scripts/diy_xmf.js >> /scripts/logs/diy_xmf.log 2>&1\n"
   printf "# interCenter渠道店铺签到\n0 0 * * * node /scripts/diy_inter_shop_sign.js >> /scripts/logs/diy_inter_shop_sign.log 2>&1\n"
   printf "# 有机牧场\n0 0,1-22/2 1-31 4-7 * node /scripts/diy_pasture.js >> /scripts/logs/diy_pasture.log 2>&1\n"
   printf "# 店铺加购有礼\n15 12 * * * node /scripts/diy_shop_add_to_car.js >> /scripts/logs/diy_shop_add_to_car.log 2>&1\n"
   printf "# 店铺关注有礼\n15 15 * * * node /scripts/diy_shop_follow_sku.js >> /scripts/logs/diy_shop_follow_sku.log 2>&1\n"
   printf "# 店铺大转盘\n3 0,10,23 * * * node /scripts/diy_shop_lottery.js >> /scripts/logs/diy_shop_lottery.log 2>&1\n"
-  printf "# TCLxLINING\n25 8 * 4-5 * node /scripts/diy_tcl_lining.js >> /scripts/logs/diy_tcl_lining.log 2>&1\n"
-  printf "# Vinda-维达品牌日\n15 08 5-30 4 * node /scripts/diy_vinda.js >> /scripts/logs/diy_vinda.log 2>&1\n"
   printf "# 半点京豆雨\n30 20-23/1 * * * node /scripts/diy_half_redrain.js >> /scripts/logs/diy_half_redrain.log 2>&1\n"
   printf "# 整点京豆雨\n1 0-23/1 * * * node /scripts/diy_super_redrain.js >> /scripts/logs/diy_super_redrain.log 2>&1\n"
   printf "# 东东超市\n59,29 23,0 * * * sleep 57; node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1\n"
   printf "# 京东汽车兑换\n0,1,3,59 23,0 * * * sleep 57; node /scripts/jd_car.js >> /scripts/logs/jd_car.log 2>&1\n"
-} >> /scripts/docker/merged_list_file.sh
+} >> ${mergedListFile}
+
+# 修改定时任务
+sed -i 's/^0,30 0 \* \* \* node \/scripts\/jd_blueCoin.js/#&/' ${mergedListFile}
+sed -i 's/^0 0 \* \* \* node \/scripts\/jd_car.js/#&/' ${mergedListFile}
+sed -i 's/^1,31 0-23/1 \* \* \* node \/scripts\/jd_live_redrain.js/#&/' ${mergedListFile}
