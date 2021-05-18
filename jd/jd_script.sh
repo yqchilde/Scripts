4f5c888aca44b04616ef95b28b9dd72ec54aa820
yqchilde <yqchilde@gmail.com>
2021-05-18 11:01:57 +0800

    资产统计通知调到9点

--- jd/jd_script.sh
+++ jd/jd_script.sh
@@ -35,7 +35,7 @@ cp $(find /ybRepo/jd/scripts/author -type f -name "*.js") /scripts/
   printf "# 店铺大转盘\n3 0,10,23 * * * node /scripts/diy_shop_lottery.js >> /scripts/logs/diy_shop_lottery.log 2>&1\n"
   printf "# 半点京豆雨\n30 16-23/1 * * * node /scripts/diy_half_redrain.js >> /scripts/logs/diy_half_redrain.log 2>&1\n"
   printf "# 整点京豆雨\n1 0-23/1 * * * node /scripts/diy_super_redrain.js >> /scripts/logs/diy_super_redrain.log 2>&1\n"
-  printf "# 京东资产变动通知\n0 8 * * * node /scripts/diy_all_bean_change.js >> /scripts/logs/diy_all_bean_change.log 2>&1\n"
+  printf "# 京东资产变动通知\n0 9 * * * node /scripts/diy_all_bean_change.js >> /scripts/logs/diy_all_bean_change.log 2>&1\n"
   printf "# 东东超市\n59,29 23,0 * * * sleep 57; node /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1\n"
   printf "# 京东汽车兑换\n0,1,3,59 23,0 * * * sleep 57; node /scripts/jd_car.js >> /scripts/logs/jd_car.log 2>&1\n"
 } >> ${mergedListFile}
