7002ba4303577696433ae9b4791a71e74a4dfc82
yqchilde <yqchilde@gmail.com>
2021-06-16 09:16:15 +0800

    Init

--- author/data/jd_fruit.js
+++ author/data/jd_fruit.js
@@ -29,7 +29,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-  '8e2785b15cef4f03a714f4288a496deb@8e2785b15cef4f03a714f4288a496deb',
+  '0f6f68f0d58d43a98d2f578a7e1a0214@8e2785b15cef4f03a714f4288a496deb',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
