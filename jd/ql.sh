fb3da61c31bbf5f00cb9611efe1e8ef1a5e0681f
yqchilde <yqchilde@gmail.com>
2021-06-17 11:19:39 +0800

    qinglong

---
+++ jd/ql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ql repo https://github.com/yqchilde/Scripts.git "jd_|jx_|diy_|getJDCookie" "backup" "^jd[^_]|USER" "jd"
+ql repo https://github.com/longzhuzhu/nianyu.git "qx" "main"
+ql repo https://github.com/whyour/hundun.git "quanx" "tokens|caiyun|didi|donate|fold|Env"
+ql repo https://github.com/Wenmoux/scripts.git "other|jd" "" "" "wen"
+ql repo https://github.com/panghu999/panghu.git "jd_"
+ql repo https://github.com/hyzaw/scripts.git "ddo_"
+ql repo https://github.com/star261/jd.git "scripts" "code"
+ql repo https://github.com/zooPanda/zoo.git "zoo"
