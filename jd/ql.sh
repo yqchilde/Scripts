2d6c740de6cf977f1c6c664f3aa61a5c80003824
yqchilde <yqchilde@gmail.com>
2021-07-01 10:38:42 +0800

    no message

--- jd/ql.sh
+++ jd/ql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ql repo https://github.com/yqchilde/Scripts.git "jd_|jx_|diy_|getJDCookie" "backup" "^jd[^_]|USER" "jd"
 ql repo https://github.com/he1pu/JDHelp.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon" "^jd[^_]|USER"
 ql repo https://github.com/longzhuzhu/nianyu.git "qx"
 ql repo https://github.com/whyour/hundun.git "quanx" "tokens|caiyun|didi|donate|fold|Env"
@@ -12,4 +13,3 @@ ql repo https://github.com/ZCY01/daily_scripts.git "jd_"
 ql repo https://github.com/curtinlv/JD-Script.git
 ql repo https://github.com/moposmall/Script.git "Me"
 ql repo https://github.com/photonmang/quantumultX.git "JDscripts"
-ql repo https://github.com/yqchilde/Scripts.git "jd_|jx_|diy_|getJDCookie" "backup" "^jd[^_]|USER" "none"
