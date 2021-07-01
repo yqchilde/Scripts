bdfbb0c77d455ba4faf48245de7f8f886f336ea5
yqchilde <yqchilde@gmail.com>
2021-07-01 09:43:59 +0800

    no message

--- jd/ql.sh
+++ jd/ql.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-ql repo https://github.com/yqchilde/Scripts.git "jd_|jx_|diy_|getJDCookie" "backup" "^jd[^_]|USER" "jd"
-ql repo https://github.com/longzhuzhu/nianyu.git "qx" "main"
+ql repo https://github.com/he1pu/JDHelp.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon" "^jd[^_]|USER"
+ql repo https://github.com/longzhuzhu/nianyu.git "qx"
 ql repo https://github.com/whyour/hundun.git "quanx" "tokens|caiyun|didi|donate|fold|Env"
 ql repo https://github.com/Wenmoux/scripts.git "other|jd" "" "" "wen"
 ql repo https://github.com/panghu999/panghu.git "jd_"
 ql repo https://github.com/hyzaw/scripts.git "ddo_"
 ql repo https://github.com/star261/jd.git "scripts" "code"
 ql repo https://github.com/zooPanda/zoo.git "" "do_not_use" "" "dev"
+ql repo https://github.com/Ariszy/Private-Script.git "JD"
+ql repo https://github.com/ZCY01/daily_scripts.git "jd_"
+ql repo https://github.com/curtinlv/JD-Script.git
+ql repo https://github.com/moposmall/Script.git "Me"
+ql repo https://github.com/photonmang/quantumultX.git "JDscripts"
