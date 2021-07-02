a260688096e15f7d25d2f08e002b5db1ff3ce1a6
yqchilde <yqchilde@gmail.com>
2021-07-02 15:36:43 +0800

    no message

--- jd/ql.sh
+++ jd/ql.sh
@@ -5,7 +5,7 @@ dir_shell=/ql/shell
 . $dir_shell/api.sh
 dir_scripts=/ql/scripts
 
-author_repo="curtinlv_JD-Script"
+author_repo="curtinlv_JD-Script panghu999_panghu Wenmoux_scripts hyzaw_scripts star261_jd zooPanda_zoo"
 
 del_cron() {
     local detail=""
@@ -49,11 +49,6 @@ ql_repo() {
   ql repo https://github.com/he1pu/JDHelp.git "jd_|jx_|getJDCookie" "activity|backUp|jd_delCoupon" "^jd[^_]|USER"
   ql repo https://github.com/longzhuzhu/nianyu.git "qx"
   ql repo https://github.com/whyour/hundun.git "quanx" "tokens|caiyun|didi|donate|fold|Env"
-  ql repo https://github.com/Wenmoux/scripts.git "other|jd" "" "" "wen"
-  ql repo https://github.com/panghu999/panghu.git "jd_"
-  ql repo https://github.com/hyzaw/scripts.git "ddo_"
-  ql repo https://github.com/star261/jd.git "scripts" "code"
-  ql repo https://github.com/zooPanda/zoo.git "" "do_not_use" "" "dev"
   ql repo https://github.com/Ariszy/Private-Script.git "JD"
   ql repo https://github.com/ZCY01/daily_scripts.git "jd_"
   ql repo https://github.com/moposmall/Script.git "Me"
