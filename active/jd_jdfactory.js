69eeafac607fc4c83612426d1227ed4c21fb68ce
yqchilde <yqchilde@gmail.com>
2021-07-03 10:45:34 +0800

    Fixed jd_factory.

--- active/jd_jdfactory.js
+++ active/jd_jdfactory.js
@@ -446,14 +446,14 @@ function jdfactory_getTaskDetail() {
             data = JSON.parse(data);
             if (data.data.bizCode === 0) {
               $.taskVos = data.data.result.taskVos;//任务列表
-              $.taskVos.map(item => {
+              $.taskVos.map(async item => {
                 if (item.taskType === 14) {
                   console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${item.assistTaskDetailVo.taskToken}\n`)
                   myInviteCode = item.assistTaskDetailVo.taskToken;
                   const submitCodeRes = await submitCode();
                   if (submitCodeRes && submitCodeRes.code === 200) {
                     console.log(`🏭东东工厂-互助码提交成功！🏭`);
-                  }else if (submitCodeRes.code === 300) {
+                  } else if (submitCodeRes.code === 300) {
                     console.log(`🏭东东工厂-互助码已提交！🏭`);
                   }
                 }
