fb54673a34b445a93e4cc5e615af995b8110bc16
yqchilde <yqchilde@gmail.com>
2021-07-13 19:44:55 +0800

    fixed

--- jd/ql.sh
+++ jd/ql.sh
@@ -7,6 +7,6 @@ dir_scripts=/ql/scripts
 
 echo "⚠️ 该文件已失效，即将进行替换以下命令"
 echo '/bin/bash -c "$(curl -fsSL https://gitee.com/yqchilde/Scripts/raw/main/jd/extra.sh)"'
-sed -i -e 's/curl https:\/\/gitee.com\/yqchilde\/Scripts\/raw\/main\/jd\/ql.sh | bash/\/bin\/bash -c "$(curl -fsSL https:\/\/gitee.com\/yqchilde\/Scripts\/raw\/main\/jd\/ql.sh)"/g' /ql/config/extra.sh
+sed -i -e 's/curl https:\/\/gitee.com\/yqchilde\/Scripts\/raw\/main\/jd\/ql.sh | bash/\/bin\/bash -c "$(curl -fsSL https:\/\/gitee.com\/yqchilde\/Scripts\/raw\/main\/jd\/extra.sh)"/g' /ql/config/extra.sh
 echo '😊 已成功替换命令为\n/bin/bash -c "$(curl -fsSL https://gitee.com/yqchilde/Scripts/raw/main/jd/extra.sh)"\n\n🙆‍♂️可忽略本次通知'
 notify '自定义脚本extra.sh命令替换通知' '😊 已成功替换命令为\n/bin/bash -c "$(curl -fsSL https://gitee.com/yqchilde/Scripts/raw/main/jd/extra.sh)"\n\n🙆‍♂️可忽略本次通知'
