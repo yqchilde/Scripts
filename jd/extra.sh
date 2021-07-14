614319010e0ba502757a6dec93c0a6c916c14d0a
yqchilde <yqchilde@gmail.com>
2021-07-14 09:16:06 +0800

    Add panghu999 repo black list rule.

--- jd/extra.sh
+++ jd/extra.sh
@@ -103,7 +103,7 @@ function exec_ql_repo() {
   ql repo https://github.com/longzhuzhu/nianyu.git "qx"
   ql repo https://github.com/whyour/hundun.git "quanx" "tokens|caiyun|didi|donate|fold|Env"
   ql repo https://github.com/ZCY01/daily_scripts.git "jd_"
-  ql repo https://github.com/panghu999/panghu.git "jd_"
+  ql repo https://github.com/panghu999/panghu.git "jd_" "jd_cfdqiqiu"
   ql repo https://github.com/smiek2221/scripts.git "jd_" "gua_wealth_island" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js"
   ql repo https://github.com/Tsukasa007/my_script.git "" "jdCookie|USER_AGENTS|sendNotify|backup|zlmjh|smzdm_mission" "" "master"
 }
